e00c45212ace72f799ce6bad4a84971dd47f671f
Alvin Chan <aschan@gmail.com>
2023-09-11 12:00:13 -0700

    Run `.setup()` for each filetype

--- lua/fido/filetypes/init.lua
+++ lua/fido/filetypes/init.lua
@@ -1,6 +1,16 @@
-return {
+local filetypes = {
   http = require('fido.filetypes.http'),
   node = require('fido.filetypes.node'),
   psql = require('fido.filetypes.psql'),
   redis = require('fido.filetypes.redis'),
 }
+
+return vim.tbl_extend('force', filetypes, {
+  setup = function()
+    for _, filetype in pairs(filetypes) do
+      if filetype.setup then
+        filetype.setup()
+      end
+    end
+  end,
+})
