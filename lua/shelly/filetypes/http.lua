215549d62f3289a281e0cd9f2c8cccb5a7731504
Alvin Chan <aschan@gmail.com>
2025-10-26 22:39:10 -0700

    Parse `curl` response headers to determine the filetype

--- lua/shelly/filetypes/http.lua
+++ lua/shelly/filetypes/http.lua
@@ -45,13 +45,94 @@ local function execute(evaluated, callback)
   end
   local command = { 'curl', '-i', '-s', '-L', '-X', method }
   vim.list_extend(command, evaluated.command_args)
+  local function handle_curl_result(result)
+    local lines = result.stdout
+    local header_blocks = {}
+    local body_lines = {}
+    local current_headers = {}
+    local in_headers = true
+    for i, line in ipairs(lines) do
+      if in_headers then
+        if line == '' then
+          if #current_headers > 0 then
+            table.insert(header_blocks, current_headers)
+            current_headers = {}
+          end
+          in_headers = false
+        else
+          table.insert(current_headers, line)
+        end
+      else
+        -- If we see another HTTP status line, start new headers (redirect)
+        if line:match('^HTTP/%d') then
+          if #current_headers > 0 then
+            table.insert(header_blocks, current_headers)
+          end
+          current_headers = { line }
+          in_headers = true
+        else
+          table.insert(body_lines, line)
+        end
+      end
+    end
+    if #current_headers > 0 then
+      table.insert(header_blocks, current_headers)
+    end
+    -- Use last header block for content-type
+    local last_headers = header_blocks[#header_blocks] or {}
+    local content_type
+    for _, h in ipairs(last_headers) do
+      local lower_h = h:lower()
+      local ct = lower_h:match('^content%-type:%s*([^;]+)')
+      if ct then
+        content_type = ct
+        break
+      end
+    end
+    local filetype_map = {
+      ['application/json'] = 'json',
+      ['text/html'] = 'html',
+      ['application/xml'] = 'xml',
+      ['text/xml'] = 'xml',
+      ['text/plain'] = 'text',
+      ['application/javascript'] = 'javascript',
+      ['text/css'] = 'css',
+      ['text/markdown'] = 'markdown',
+    }
+    local result_filetype = filetype_map[content_type or ''] or nil
+    -- If -i or --include is present, prepend all headers to stdout
+    local include_headers = false
+    for _, arg in ipairs(evaluated.command_args) do
+      if arg == '-i' or arg == '--include' then
+        include_headers = true
+        break
+      end
+    end
+    local final_stdout = {}
+    if include_headers then
+      for _, block in ipairs(header_blocks) do
+        for _, h in ipairs(block) do
+          table.insert(final_stdout, h)
+        end
+        table.insert(final_stdout, '')
+      end
+      for _, b in ipairs(body_lines) do
+        table.insert(final_stdout, b)
+      end
+    else
+      for _, b in ipairs(body_lines) do
+        table.insert(final_stdout, b)
+      end
+    end
+    callback({ stdout = final_stdout, stderr = result.stderr, filetype = result_filetype })
+  end
   if method == 'GQL' then
     table.insert(command, url)
     table.insert(command, '-H')
     table.insert(command, 'Content-Type: application/json')
     table.insert(command, '--data')
     table.insert(command, vim.json.encode({ query = table.concat(body_lines, '\n') }))
-    utils.execute_shell(command, callback)
+    utils.execute_shell(command, handle_curl_result)
     return
   end
   if (method == 'POST' or method == 'PUT') and #body_lines > 0 then
@@ -72,7 +153,7 @@ local function execute(evaluated, callback)
         end
       end
     end
-    utils.execute_shell(command, callback)
+    utils.execute_shell(command, handle_curl_result)
     return
   end
   if (method == 'GET' or method == 'DELETE') and #body_lines > 0 then
@@ -88,7 +169,7 @@ local function execute(evaluated, callback)
     end
   end
   table.insert(command, url)
-  utils.execute_shell(command, callback)
+  utils.execute_shell(command, handle_curl_result)
 end
 
 return { execute = execute }
