d9d6dfe2419eeec6a99c0f824d11b3c46b597e24
Alvin Chan <aschan@gmail.com>
2025-11-04 14:12:07 -0800

    Remove parsing connection details from the dictionary

--- lua/shelly/filetypes/postgresql.lua
+++ lua/shelly/filetypes/postgresql.lua
@@ -8,40 +8,23 @@ local function execute(evaluated, callback)
       callback({ stdout = {}, stderr = { 'No SQL to execute' } })
     end)
   end
+
   local command = { 'psql' }
-  local sql = table.concat(evaluated.lines, '\n')
-  local connection_string = nil
+
+  local connection_string
   for _, url in ipairs(evaluated.urls) do
     if url:match('^postgres[ql]*://') then
       connection_string = url
     end
   end
+
   if not connection_string then
-    connection_string = evaluated.dictionary.url
-      or evaluated.dictionary.connection_string
-      or evaluated.dictionary.database_url
-      or evaluated.dictionary.dsn
+    return vim.schedule(function()
+      callback({ stdout = {}, stderr = { 'No connection string provided' } })
+    end)
   end
-  if connection_string then
-    table.insert(command, connection_string)
-  else
-    if evaluated.dictionary.host then
-      table.insert(command, '-h')
-      table.insert(command, evaluated.dictionary.host)
-    end
-    if evaluated.dictionary.port then
-      table.insert(command, '-p')
-      table.insert(command, evaluated.dictionary.port)
-    end
-    if evaluated.dictionary.username or evaluated.dictionary.user then
-      table.insert(command, '-U')
-      table.insert(command, evaluated.dictionary.username or evaluated.dictionary.user)
-    end
-    if evaluated.dictionary.database or evaluated.dictionary.dbname then
-      table.insert(command, '-d')
-      table.insert(command, evaluated.dictionary.database or evaluated.dictionary.dbname)
-    end
-  end
+
+  table.insert(command, connection_string)
   vim.list_extend(command, evaluated.command_args)
   utils.execute_shell(command, { stdin = evaluated.lines, shelly_args = evaluated.shelly_args }, callback)
 end
